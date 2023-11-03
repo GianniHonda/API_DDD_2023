b977cec5a34322938aa25d812e36105c419a32e4
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2023-11-03 16:10:30 +0900

    Domain

---
+++ Dominio/Interfaces/Genericos/IGenericos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Genericos
+{
+    public interface IGenericos<T> where T : class
+    {
+        Task Adicionar(T objeto);
+
+        Task Atualizar(T objeto);
+
+        Task Excluir(T objeto);
+
+        Task<T> BuscarPortId(int Id);
+
+        Task<List<T>> Listar();
+    }
+}
---
+++ Dominio/Interfaces/InterfacesServicos/IServicoNoticia.cs
@@ -0,0 +1,18 @@
+﻿using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.InterfacesServicos
+{
+    public interface IServicoNoticia
+    {
+        Task AdicionaNoticia(Noticia noticia);
+
+        Task AtualizaNoticia(Noticia noticia);
+
+        Task<List<Noticia>> ListarNoticiasAtivas();
+    }
+}
